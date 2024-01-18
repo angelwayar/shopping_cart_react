5420f19c51d278f0baee788c15ff976e1bdbf6bd
amway <angelmanuelwayar@gmail.com>
2024-01-18 11:30:59 -0400

    firts commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export function Footer() {
+    return (
+        <footer className="footer">
+            <h4>
+                Prueba técnica de React 
+                <span> @angelWayar</span>
+            </h4>
+            <h5>Shopping Cart con useContext & useReducer</h5>
+        </footer>
+    )
+}
---
+++ src/context/cart.jsx
@@ -0,0 +1,34 @@
+import { createContext, useReducer, useState } from "react";
+import { cartReducer, initialState } from "../reducers/cart";
+
+export const CartContext = createContext()
+
+
+
+export function CartProvider({ children }) {
+    const [state, dispatch] = useReducer(cartReducer, initialState)
+
+    const addToCart = product => dispatch({
+        type: 'ADD_TO_CART',
+        payload: product
+    })
+
+    const removeFromCart = product => dispatch({
+        type: 'REMOVE_FROM_CART',
+        payload: product
+    })
+
+    const clearCart = () => dispatch({ type: 'CLEAN_CART' })
+
+    return (
+        <CartContext.Provider
+            value={{
+                cart: state,
+                addToCart,
+                removeFromCart,
+                clearCart
+            }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
